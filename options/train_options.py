4a2b5e26a8a91f6b516dc482ceaf6276d2a4c7b2
Maham Noor <132357536+mahamnoor06@users.noreply.github.com>
2024-02-17 04:21:14 +0500

    Update

--- options/train_options.py
+++ options/train_options.py
@@ -3,7 +3,7 @@ from .base_options import BaseOptions
 class TrainOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
-        # for displays
+        #  for displays
         self.parser.add_argument('--display_freq', type=int, default=99, help='frequency of showing training results on screen')
         self.parser.add_argument('--print_freq', type=int, default=100, help='frequency of showing training results on console')
         self.parser.add_argument('--save_latest_freq', type=int, default=10000, help='frequency of saving the latest results')
